b5199e84abc87fabab8438a73b636f2a1e186d3e
Daniel Apostolov <d.kaloyanoff@gmail.com>
2024-12-18 20:21:05 +0200

    Add car-details and car-create components

---
+++ autohub/src/app/features/car/car-create/car-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-create.component.html',
+  styleUrl: './car-create.component.css'
+})
+export class CarCreateComponent {
+
+}
