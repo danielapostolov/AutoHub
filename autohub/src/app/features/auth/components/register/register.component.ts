8aade8e0ce67b24c9ca129dadc252ea934f65cd0
Daniel Apostolov <d.kaloyanoff@gmail.com>
2024-12-19 21:12:57 +0200

    Validate register and car-create

--- autohub/src/app/features/auth/components/register/register.component.ts
+++ autohub/src/app/features/auth/components/register/register.component.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../../service/user.service';
+import { emailValidator } from '../../../../utils/email.validator';
+import { matchPasswordsValidator } from '../../../../utils/matchPasswordsValidator';
+import { DOMAINS } from '../../../../constants';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    email: new FormControl('', [Validators.required, emailValidator(DOMAINS)]),
+    tel: new FormControl(''),
+    passGroup: new FormGroup(
+      {
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(5),
+        ]),
+        rePassword: new FormControl('', [Validators.required]),
+      },
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')],
+      }
+    ),
+  });
 
+  constructor(private userService: UserService, private router: Router) { }
+
+  get passGroup() {
+    return this.form.get('passGroup');
+  }
+
+  register() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    // const {
+    //   username,
+    //   email,
+    //   tel,
+    //   passGroup: { password, rePassword } = {},
+    // } = this.form.value;
+
+    // this.userService
+    //   .register(username!, email!, tel!, password!, rePassword!)
+    //   .subscribe(() => {
+    //     this.router.navigate(['/themes']);
+    //   });
+  }
 }
--- autohub/src/app/features/car/car-create/car-create.component.ts
+++ autohub/src/app/features/car/car-create/car-create.component.ts
@@ -1,27 +1,49 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ApiService } from '../../../api.service';
 
 @Component({
   selector: 'app-car-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './car-create.component.html',
   styleUrl: './car-create.component.css'
 })
 export class CarCreateComponent {
+  carForm = new FormGroup({
+    make: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    model: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    year: new FormControl('', [
+      Validators.required,
+      Validators.min(1900),
+      Validators.max(new Date().getFullYear())
+    ]),
+    imgUrl: new FormControl('', [Validators.required, Validators.pattern(/https?:\/\/.+/)]),
+    mileage: new FormControl('', [Validators.required, Validators.min(0)]),
+    fuelType: new FormControl('', [Validators.required]),
+    price: new FormControl('', [
+      Validators.required,
+      Validators.min(1)
+    ]),
+    located: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  });
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private router: Router) { }
 
-  createCar(event: Event, make: string,
-    model: string,
-    year: string,
-    imgUrl: string,
-    mileage: string,
-    fuelType: string,
-    price: string,
-    located: string) {
+  createCar() {
+    if (this.carForm.invalid) {
+      return;
+    }
 
+    // Submit form data
+    // this.api.addCar(this.carForm.value).subscribe({
+    //   next: () => this.router.navigate(['/cars']),
+    //   error: (err) => console.error('Error creating car:', err)
+    // });
+  }
 
-    event.preventDefault();
+  get formControls() {
+    return this.carForm.controls;
   }
 }
